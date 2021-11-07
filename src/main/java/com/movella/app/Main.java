a4424e3ade3ff0f115b8d4be6d6fba8c8f733428
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2021-11-07 11:40:02 -0300

    session, navbar

--- src/main/java/com/movella/app/Main.java
+++ src/main/java/com/movella/app/Main.java
@@ -12,6 +12,8 @@ import com.movella.service.ContatoService;
 import com.movella.service.UsuarioService;
 
 import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 import spark.Session;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
@@ -32,49 +34,58 @@ public class Main {
       });
 
       notFound((req, res) -> {
-        return render(new HashMap<>(), "/not-found");
+        return render(populateHashMap(new HashMap<>(), req), "/not-found");
       });
 
       internalServerError((req, res) -> {
-        return render(new HashMap<>(), "/error");
+        return render(populateHashMap(new HashMap<>(), req), "/error");
       });
 
       get("/", (req, res) -> {
-        return render(new HashMap<>(), "/moveis");
+        return render(populateHashMap(new HashMap<>(), req), "/moveis");
       });
 
       get("/moveis", (req, res) -> {
-        return render(new HashMap<>(), "/moveis");
+        return render(populateHashMap(new HashMap<>(), req), "/moveis");
       });
 
       get("/sobre", (req, res) -> {
-        return render(new HashMap<>(), "/sobre");
+        return render(populateHashMap(new HashMap<>(), req), "/sobre");
       });
 
       get("/contato", (req, res) -> {
-        return render(new HashMap<>(), "/contato");
+        return render(populateHashMap(new HashMap<>(), req), "/contato");
       });
 
       get("/perfil", (req, res) -> {
-        return render(new HashMap<>(), "/perfil");
+        if (blockAccess(req, res)) {
+          res.redirect("/entrar");
+          return null;
+        }
+
+        return render(populateHashMap(new HashMap<>(), req), "/perfil");
       });
 
       get("/entrar", (req, res) -> {
-        return render(new HashMap<>(), "/entrar");
+        return render(populateHashMap(new HashMap<>(), req), "/entrar");
       });
 
       get("/criarconta", (req, res) -> {
-        return render(new HashMap<>(), "/criarconta");
+        return render(populateHashMap(new HashMap<>(), req), "/criarconta");
       });
 
+      get("/sair", (req, res) -> {
+        req.session().invalidate();
+
+        res.redirect("/entrar");
+
+        return render(populateHashMap(new HashMap<>(), req), "/entrar");
+      });
+
+      // API
+
       before("/api/*", (req, res) -> {
-        final String splat = req.splat()[0];
-
-        if (splat.equals("login") || splat.equals("register") || splat.equals("contato/create"))
-          return;
-
-        if (req.session().attribute("user") == null)
-          new Unauthorized(res);
+        blockAccess(req, res);
       });
 
       path("/api", () -> {
@@ -91,6 +102,32 @@ public class Main {
     } catch (Exception e) {
       System.out.println(e);
     }
+  }
+
+  static HashMap<String, Object> populateHashMap(HashMap<String, Object> hashMap, Request req) {
+    final Session session = req.session();
+    final boolean hasSession = session.attribute("user") != null;
+
+    hashMap.put("hasSession", hasSession);
+    hashMap.put("user", session.attribute("user"));
+
+    return hashMap;
+  }
+
+  static boolean blockAccess(Request req, Response res) {
+    final String path = req.pathInfo();
+    final boolean hasSession = req.session().attribute("user") != null;
+
+    if (path.equals("/api/login") || path.equals("/api/register") || path.equals("/api/contato/create"))
+      return false;
+
+    if (path.equals("/perfil") && !hasSession)
+      return true;
+
+    if (!hasSession && path.contains("/api/"))
+      new Unauthorized(res);
+
+    return false;
   }
 
   static int getHerokuAssignedPort() {
